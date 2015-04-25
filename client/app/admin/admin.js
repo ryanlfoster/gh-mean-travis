303de23825ac21b17570e7b429a9ccdf582674ca
Lothar Wieske <lothar.wieske@web.de>
2015-04-25 11:44:19 +0200

    yo angular-fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ghMeanTravisApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
